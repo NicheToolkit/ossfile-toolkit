f92dccb9ef672d323bcf0989216daffa581755bc
Cyan <snow22314@outlook.com>
2025-11-21 13:54:14 +0800

    fix

--- ossfile-toolkit-core/src/main/java/io/github/nichetoolkit/ossfile/configure/OssfileCoreAutoConfigure.java
+++ ossfile-toolkit-core/src/main/java/io/github/nichetoolkit/ossfile/configure/OssfileCoreAutoConfigure.java
@@ -1,12 +1,10 @@
 package io.github.nichetoolkit.ossfile.configure;
 
-import io.github.nichetoolkit.rest.RestI18n;
+import io.github.nichetoolkit.rest.resource.RestI18nResources;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Collections;
 
 /**
  * <code>OssfileCoreAutoConfigure</code>
@@ -39,15 +37,15 @@ public class OssfileCoreAutoConfigure {
     }
 
     /**
-     * <code>ossfileI18nBasename</code>
-     * <p>The ossfile i 18 n basename method.</p>
-     * @return {@link io.github.nichetoolkit.rest.RestI18n} <p>The ossfile i 18 n basename return object is <code>RestI18n</code> type.</p>
-     * @see io.github.nichetoolkit.rest.RestI18n
+     * <code>ossfileI18nResources</code>
+     * <p>The ossfile i 18 n resources method.</p>
+     * @return {@link io.github.nichetoolkit.rest.resource.RestI18nResources} <p>The ossfile i 18 n resources return object is <code>RestI18nResources</code> type.</p>
+     * @see io.github.nichetoolkit.rest.resource.RestI18nResources
      * @see org.springframework.context.annotation.Bean
      */
     @Bean
-    public RestI18n ossfileI18nBasename() {
-        return () -> Collections.singleton(OSSFILE_I18N);
+    public RestI18nResources ossfileI18nResources() {
+        return RestI18nResources.of(OSSFILE_I18N);
     }
 
 }
